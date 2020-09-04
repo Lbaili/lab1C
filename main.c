6a4712099ae62218d9756f46deb3b7461df50dea
Chang Li <1295118952@qq.com>
2020-09-04 12:04:50 +0000

    style

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
+// Author: Chang Li cxl5844@psu.edu
+// Collaborator: Yangling Wang yuw17@psu.edu
+
 #include <stdio.h>
-#include<readline/readline.h>
-#include<stdlib.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
 
 int main(void) {
-char *ctemp=readline("Enter temperature in celsius: " );
-double c= atof(ctemp);
-double f=c*1.8+32.0;  
-printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n",c, f);
-
-return 0;
+  char *ctemp = readline("Enter temperature in celsius: " );
+  double c = atof(ctemp);
+  double f=  c*1.8+32.0;  
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n",c, f);
+  return 0;
 }
